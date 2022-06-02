7444a018b566c4fa4da37990fd2ccec45f3ccad5
Andrés Raigoza <81978983+andresr774@users.noreply.github.com>
2022-06-02 18:49:12 -0500

    - Modified Animation example

--- DiceRolling/Tests/AnimationExample.swift
+++ DiceRolling/Tests/AnimationExample.swift
@@ -8,22 +8,35 @@
 import SwiftUI
 
 struct AnimationExample: View {
-    @State var degrees = 0.0
-    @State var offset = 0.0
+    @State private var redDegrees = 0.0
+    @State private var redOffset = 0.0
+    @State private var blueDegrees = -90.0
+    @State private var blueOffset = 150.0
     
     var body: some View {
         VStack {
-            Button("Rotate") {
-                withAnimation(.linear(duration: 1)) {
-                    degrees = 90
-                    offset = -150
+            ZStack {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 150, height: 150)
+                    .rotation3DEffect(.degrees(redDegrees), axis: (x: 0, y: -1, z: 0), anchor: .trailing, perspective: 0.1)
+                    .offset(x: redOffset, y: 0)
+                
+                Rectangle()
+                    .fill(.blue)
+                    .frame(width: 150, height: 150)
+                    .rotation3DEffect(.degrees(blueDegrees), axis: (x: 0, y: -1, z: 0), anchor: .leading, perspective: 0.1)
+                    .offset(x: blueOffset, y: 0)
+            }
+            
+            Button("Rotate Horizontally") {
+                withAnimation(.linear(duration: 1).repeatForever(autoreverses: true)) {
+                    redDegrees = 90
+                    redOffset = -150
+                    blueDegrees = 0
+                    blueOffset = 0
                 }
             }
-            Rectangle()
-                .fill(.red)
-                .frame(width: 150, height: 150)
-                .rotation3DEffect(.degrees(degrees), axis: (x: 0, y: -1, z: 0), anchor: .trailing, perspective: 0.1)
-                .offset(x: offset, y: 0)
         }
     }
 }
